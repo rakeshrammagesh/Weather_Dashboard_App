f28d4c8d40dc4d0b962118b8d70c85ba30071ef8
Rakesh Ram Magesh <rakeshrammagesh95@gmail.com>
2024-09-20 01:55:07 +0530

    Update main.py
    Dashboard with fake data is created

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import plotly.express as px
 
 st.title("Weather Forecast for the next few days")
 place = st.text_input("Place:  ")
@@ -8,3 +9,9 @@ st.subheader(f" {option} for the next {days} days in {place}")
 
 
 #use the command prompt option in the terminal press right click
+
+
+dates = ["2022-25-10", "2022-26-10", "2022-27-10"]
+temperatures = [10, 11, 15]
+figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature(c)"})
+st.plotly_chart(figure)
